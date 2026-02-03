70bd37cecc3258279522e146c90812435b3c4898
AK40084384 <akshayanand2121@gmail.com>
2026-02-03 18:05:21 +0530

    changes

---
+++ src/main/java/com/apps/quantityMeasurement/Length.java
@@ -0,0 +1,60 @@
+package com.apps.quantityMeasurement;
+
+import java.util.Objects;
+
+public class Length {
+
+    private double value;
+    private LengthUnit unit;
+
+    public enum LengthUnit {
+        FEET(12.0),
+        INCHES(1.0),
+        YARDS(36.0),
+        CENTIMETERS(0.393701);
+
+        private final double conversionFactor;
+
+        LengthUnit(double conversionFactor) {
+            this.conversionFactor = conversionFactor;
+        }
+
+        public double getConversionFactor() {
+            return conversionFactor;
+        }
+
+        public double toUnit(double value) {
+            return value * conversionFactor;
+        }
+    }
+
+    // Constructor to initialize length value and unit
+    public Length(Double value, LengthUnit unit) {
+        if ( unit== null){
+         throw new IllegalArgumentException("Value cannot be null");
+        }
+        this.value = value;
+        this.unit = unit;
+    }
+
+    // Convert the length value to the base unit (inches)
+    private double convertToBaseUnit() {
+        return unit.toUnit(value);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Length)) return false;
+        Length someObj = (Length) obj;
+//        System.out.println(this.value + " both" + someObj.value);
+//        System.out.println(this.convertToBaseUnit() + " both" + someObj.convertToBaseUnit());
+        return Double.compare(this.convertToBaseUnit(), someObj.convertToBaseUnit()) == 0;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, unit);
+    }
+}
--- src/main/java/com/apps/quantityMeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantityMeasurement/QuantityMeasurementApp.java
@@ -1,5 +1,58 @@
 package com.apps.quantityMeasurement;
 
+import static com.apps.quantityMeasurement.Length.*;
+
+
 public class QuantityMeasurementApp {
 
+    public static void demonstrateFeetAndInchesEquality() {
+        Length length1 = new Length(1.0, Length.LengthUnit.FEET);
+        Length length2 = new Length(12.0, Length.LengthUnit.INCHES);
+        System.out.println("Are lengths equal for Feet And Inches Equalty? " + length1.equals(length2)); // Should print true
+    }
+
+    public static void demonstrateFeetEquality() {
+        Length length1 = new Length(12.0, Length.LengthUnit.FEET);
+        Length length2 = new Length(12.0, Length.LengthUnit.FEET);
+        System.out.println("Are lengths equal for Feet Equality? " + length1.equals(length2));
+    }
+
+
+    public static void demonstrateInchesEquality() {
+        Length length1 = new Length(12.0, Length.LengthUnit.INCHES);
+        Length length2 = new Length(12.0, Length.LengthUnit.INCHES);
+        System.out.println("Are lengths equal for Inches Equality? " + length1.equals(length2));
+    }
+
+    public static void main(String[] args) {
+
+//        demonstrateFeetEquality();
+//        demonstrateInchesEquality();
+//        demonstrateFeetAndInchesEquality();
+
+
+        // Demonstrate Feet and Inches comparison
+        demonstrateLengthComparison(1.0, Length.LengthUnit.FEET, 12.0, Length.LengthUnit.INCHES);
+
+        // Demonstrate Yards and Inches comparison
+        demonstrateLengthComparison(1.0, Length.LengthUnit.YARDS, 36.0, Length.LengthUnit.INCHES);
+
+        // Demonstrate Centimeters and Inches comparison
+        demonstrateLengthComparison(100.0, Length.LengthUnit.CENTIMETERS, 39.3701, Length.LengthUnit.INCHES);
+
+        // Demonstrate Feet and Yards comparison
+        demonstrateLengthComparison(3.0, Length.LengthUnit.FEET, 1.0, Length.LengthUnit.YARDS);
+
+        // Demonstrate Centimeters and Feet comparison
+        demonstrateLengthComparison(30.48, Length.LengthUnit.CENTIMETERS, 1.0, Length.LengthUnit.FEET);
+
+    }
+
+    private static void demonstrateLengthComparison(double v, LengthUnit lengthUnit, double v1, LengthUnit lengthUnit1) {
+        if (lengthUnit != null && lengthUnit1 != null) {
+            boolean status = (new Length(v, lengthUnit).equals(new Length(v1, lengthUnit1))) ? true : false;
+            System.out.println("Equality status for " + v + " " + lengthUnit + " and " + v1+ " " + lengthUnit1 + " : " + status);
+        }
+    }
 }
+
