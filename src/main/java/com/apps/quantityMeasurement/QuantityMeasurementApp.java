56de650848f56a19fa48f222d46a08bf542c5214
AK40084384 <akshayanand2121@gmail.com>
2026-02-03 15:41:09 +0530

    changes

--- src/main/java/com/apps/quantityMeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantityMeasurement/QuantityMeasurementApp.java
@@ -1,5 +1,32 @@
 package com.apps.quantityMeasurement;
 
+import static com.apps.quantityMeasurement.Length.*;
+
 public class QuantityMeasurementApp {
 
+    public static void demonstrateFeetAndInchesEquality () {
+        Length length1 = new Length(1.0, Length.LengthUnit.FEET);
+        Length length2 = new Length(12.0, Length.LengthUnit.INCHES);
+        System.out.println("Are lengths equal for Feet And Inches Equalty? " + length1.equals(length2)); // Should print true
+    }
+
+    public static void demonstrateFeetEquality () {
+        Length length1 = new Length(12.0, Length.LengthUnit.FEET);
+        Length length2 = new Length(12.0, Length.LengthUnit.FEET);
+        System.out.println("Are lengths equal for Feet Equality? " + length1.equals(length2));
+    }
+
+
+    public static void demonstrateInchesEquality () {
+        Length length1 = new Length(12.0, Length.LengthUnit.INCHES);
+        Length length2 = new Length(12.0, Length.LengthUnit.INCHES);
+        System.out.println("Are lengths equal for Inches Equality? " + length1.equals(length2));
+    }
+
+    public static void main(String[] args) {
+
+        demonstrateFeetEquality();
+        demonstrateInchesEquality();
+        demonstrateFeetAndInchesEquality();
+    }
 }
--- src/test/java/com/apps/quantityMeasurementApp/Test/QuantityMeasurementAppTest.java
+++ src/test/java/com/apps/quantityMeasurementApp/Test/QuantityMeasurementAppTest.java
@@ -1,4 +1,83 @@
 package com.apps.quantityMeasurementApp.Test;
 
+import com.apps.quantityMeasurement.Length;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class QuantityMeasurementAppTest {
+
+    @Test
+    public void testFeetEquality_SameValue() {
+        Length oneFeet = new Length(1.0, Length.LengthUnit.FEET);
+        Length anotherFeet = new Length(1.0, Length.LengthUnit.FEET);
+        assertEquals(oneFeet, anotherFeet);
+    }
+
+    @Test
+    public void testInchesEquality_SameValue() {
+        Length oneFeet = new Length(1.0, Length.LengthUnit.INCHES);
+        Length anotherFeet = new Length(1.0, Length.LengthUnit.INCHES);
+        assertEquals(oneFeet, anotherFeet);
+    }
+
+
+    @Test
+    public void testFeetEquality_NullComparison() {
+        Length oneFeet = new Length(1.0, Length.LengthUnit.FEET);
+        assertNotEquals(null, oneFeet);
+    }
+
+    @Test
+    public void testInchToFeetEquality_EquivalentValue() {
+        Length oneFeet = new Length(12.0, Length.LengthUnit.INCHES);
+        Length anotherFeet = new Length(1.0, Length.LengthUnit.FEET);
+        assertEquals(oneFeet, anotherFeet);
+    }
+
+    @Test
+    public void testFeetEquality_DifferentValue() {
+        Length oneFeet = new Length(1.0, Length.LengthUnit.FEET);
+        Length anotherFeet = new Length(5.0, Length.LengthUnit.FEET);
+        assertNotEquals(oneFeet, anotherFeet);
+    }
+
+    @Test
+    public void testInchesEquality_DifferentValue() {
+        Length oneFeet = new Length(1.0, Length.LengthUnit.INCHES);
+        Length anotherFeet = new Length(3.0, Length.LengthUnit.INCHES);
+        assertNotEquals(oneFeet, anotherFeet);
+    }
+
+    @Test
+    public void testEquality_InvalidUnit() {
+        Length oneFeet = new Length(1.0, Length.LengthUnit.INCHES);
+        String s = "Hello";
+        assertNotEquals(s, oneFeet);
+    }
+
+    @Test
+    public void testEquality_NullUnit() {
+        try{
+            Length nullLengthUnit= new Length(1.0,null);
+            Length oneInches = new Length(99.0, Length.LengthUnit.INCHES);
+            assertNotEquals(nullLengthUnit,oneInches);
+        }catch (Exception e){
+            assertNotNull(e);
+        }
+    }
+
+    @Test
+    public void testInchesEquality_SameReference() {
+        Length oneInches = new Length(1.0, Length.LengthUnit.INCHES);
+        assertEquals(oneInches, oneInches);
+    }
+
+    @Test
+    public void testInchesEquality_NullComparison() {
+        Length oneFeet = new Length(1.0, Length.LengthUnit.INCHES);
+        assertNotEquals(null, oneFeet);
+    }
+
+
 }
