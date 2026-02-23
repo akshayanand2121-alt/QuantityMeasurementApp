a0f58c6e689c1202d9488c29a2d7fd7623f823f2
Akshay <akshayanand2121@gmail.com>
2026-02-23 12:40:09 +0530

    UC6 complete with test cases

--- src/main/java/com/apps/quantityMeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantityMeasurement/QuantityMeasurementApp.java
@@ -1,5 +1,18 @@
 package com.apps.quantityMeasurement;
+import com.apps.quantityMeasurement.Length;
 
 public class QuantityMeasurementApp {
 
+    public static void main(String[] args) {
+
+        additionDemonstration(new Length(1, Length.LengthUnit.FEET),new Length(12, Length.LengthUnit.INCHES), Length.LengthUnit.FEET);
+
+    }
+
+    public static Length additionDemonstration(Length lengthOne, Length lengthTwo, Length.LengthUnit targetUnit) {
+        Length result = Length.add(lengthOne,lengthTwo,targetUnit);
+        System.out.println("Addition of " + lengthOne.getValue() + " "+ lengthOne.getUnit() + " and " + lengthTwo.getValue() +" "+ lengthTwo.getUnit()+ " is " + result.getValue() + " "+result.getUnit());
+        //  Length lengthResult = new Length(result,lengthOne.getUnit());
+        return result;
+    }
 }
